6501551750efbf6fffc735dcc69bdd4ad22f36ff
BertrandPouget <markhillsxd@gmail.com>
2024-03-13 22:18:47 +0100

    remove auth

--- litpi2_auth.py
+++ litpi2_auth.py
@@ -24,15 +24,50 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 df_chores = conn.read(worksheet="Chores", usecols=list(range(0,5)),nrows=14, ttl=1)
 df_shopping = conn.read(worksheet="Shopping", usecols=list(range(0,1)), ttl=1)
 df_debts = conn.read(worksheet="Debts", usecols=list(range(0,2)), nrows=3, ttl=1)
-df_passwords = conn.read(worksheet="Passwords", usecols=list(range(0,2)), nrows=3, ttl=1) 
 
 #0. Authentication
-st.title("Benvenuto su Litpi:house:")
 fighters = ['Andrea', 'Marco', 'Martino']
 
-fighter = st.radio(label = "Chi sei?",
-                   options = ("Andrea", "Marco", "Martino"),
-                   index = None)
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.title("Accedi a LitPi🏡")
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            st.session_state["fighter"] = st.session_state["username"]
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+if not check_password():
+    st.stop()
+
+st.title(f"Benvenuto {st.session_state['fighter']}!")
 
 selected = option_menu(
     menu_title = None,
