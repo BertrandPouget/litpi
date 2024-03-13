8cec28b1b3ef2824115cb127eb909ab471ae71f4
BertrandPouget <markhillsxd@gmail.com>
2024-03-13 12:01:13 +0100

    Update litpi2.py

--- litpi2.py
+++ litpi2.py
@@ -6,7 +6,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Set page configuration
-st.set_page_config(page_title="LitPi", page_icon="🏡")
+st.set_page_config(page_title="LitPi")
 
 # Establish connection to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
@@ -36,6 +36,15 @@ selected = option_menu(
 
 # 1. Chores
 if selected == 'Pulizie':
+    st.markdown("## Classifica")
+    points = df_chores.copy()
+    points[fighters] = points[fighters].multiply(df_chores['Valore'], axis=0)
+    rank = points[fighters].sum().sort_values(ascending=False)
+
+    for i, (fighter, score) in enumerate(rank.items(), 1):
+        medal = ":first_place_medal:" if i == 1 else ":second_place_medal:" if i == 2 else ":third_place_medal:" if i == 3 else ""
+        st.markdown(f"{medal} **{fighter}** - *{score} punti*")
+
     st.markdown("## Aggiornamento")
     chores = st.multiselect(label = "Seleziona i compiti che hai fatto",
                         options = df_chores.loc[:, "Compito"].tolist(),
@@ -49,15 +58,6 @@ if selected == 'Pulizie':
         conn.update(worksheet="Chores", data=new_df_chores)
         progress_message.text("Aggiornamento completato!\nRicarica la pagina per vedere i risultati.")
 
-    st.markdown("## Classifica")
-    points = df_chores.copy()
-    points[fighters] = points[fighters].multiply(df_chores['Valore'], axis=0)
-    rank = points[fighters].sum().sort_values(ascending=False)
-
-    for i, (fighter, score) in enumerate(rank.items(), 1):
-        medal = ":first_place_medal:" if i == 1 else ":second_place_medal:" if i == 2 else ":third_place_medal:" if i == 3 else ""
-        st.markdown(f"{medal} **{fighter}** - *{score} punti*")
-
     st.markdown("## Tabella Completa")
     def light_blue_columns(val):
         color = '#f3fafe'
@@ -68,15 +68,16 @@ if selected == 'Pulizie':
 
 # 2. Shopping List
 if selected == 'Spesa':
-    st.markdown("## Lista della spesa")
+
     shopping_list = df_shopping['Spesa'].dropna().tolist()
+    st.markdown("## Lista")
     if shopping_list == []:
         st.markdown("La lista della spesa è vuota")
     else:
         for i, item in enumerate(shopping_list):
             st.markdown(f"{i+1}. {item}")
 
-    st.markdown("#### Aggiungi un elemento")
+    st.markdown("## Aggiornamento")
     user_input = st.text_input("Aggiungi un elemento alla lista:")
     if st.button("Aggiungi"):
         progress_message = st.text("Aggiunta elemento...")
@@ -85,11 +86,9 @@ if selected == 'Spesa':
         conn.update(worksheet="Shopping", data=new_df_shopping)
         progress_message.text("Elemento aggiunto!\nRicarica la pagina per vedere i risultati.")
 
-    st.markdown("#### Elimina singoli elementi")
     elements_to_delete = st.multiselect(label = "Seleziona gli elementi da eliminare",
                                         options = shopping_list,
-                                        default = None)
-    
+                                        default = None)    
     if st.button("Elimina"):
         progress_message = st.text("Eliminazione elementi...")
         new_df_shopping = df_shopping.copy(deep=True)
@@ -98,7 +97,6 @@ if selected == 'Spesa':
         conn.update(worksheet="Shopping", data=new_df_shopping)
         progress_message.text("Elementi eliminati!\nRicarica la pagina per vedere i risultati.")
     
-    st.markdown("#### Svuota la lista")
     if st.button("Svuota"):
         progress_message = st.text("Svuotamento lista...")
         new_df_shopping = df_shopping.copy(deep=True)
@@ -111,6 +109,9 @@ if selected == 'Spesa':
 # 3. Debts and Credits
 if selected == 'Debiti':
     debts = df_debts['Soldi'].tolist()
+    st.markdown("## Saldi")
+    for (fighter, debt) in zip(fighters, debts):
+        st.metric(label = 'ciao', value=fighter, delta=round(debt,2), label_visibility='collapsed')
 
     st.markdown("## Aggiornamento")
     credit = st.number_input(label = "Quanto hai pagato?", step = 1.00)
@@ -127,6 +128,3 @@ if selected == 'Debiti':
             new_df_debts.loc[new_df_debts['Persona'] == debtor, 'Soldi'] += debito
         conn.update(worksheet="Debts", data=new_df_debts)
         progress_message.text("Aggiornamento completato!\nRicarica la pagina per vedere i risultati.")
-
-    for (fighter, debt) in zip(fighters, debts):
-        st.metric(label = 'ciao', value=fighter, delta=round(debt,2), label_visibility='collapsed')
